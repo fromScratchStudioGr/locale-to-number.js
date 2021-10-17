711a9daa891b96be59c2561bdece0125ef49acea
George Kamtziridis <georgekam96@gmail.com>
2021-10-17 20:32:25 +0300

    Added core functionality.

--- src/core/core.js
+++ src/core/core.js
@@ -1,3 +1,5 @@
+const localeMapper = require('./locale-mapper');
+
 /**
  * Constructions the regular expression that can be used to match
  * number which use the given thousands and decimal separators.
@@ -9,3 +11,46 @@
 function constructExtractionRegex(thousandsSeparator, decimalSeparator) {
    return new RegExp(`^([+|-])?[1-9]{1}[0-9]{0,2}(${thousandsSeparator}([0-9]{3}))*(${decimalSeparator}[0-9]+)?$`);
 }
+
+/**
+ * Cleans the number representation in order to match
+ * the decimal literal format.
+ * @param {string} numberRepresentation - The initial number representation. 
+ * @param {RegExp} regex - The regular expression that corresponds to `numberRepresentation`'s locale.
+ * @param {Object} localeConfiguration - The locale configuration.
+ * @returns The representation of the number in a string format.
+ */
+function cleanNumberRepresentation(numberRepresentation, regex, localeConfiguration) {
+   if (numberRepresentation.match(regex)) {
+      // Removing the `thousands` separator and making
+      // sure the `decimal` separator is the `dot`.
+      const cleanNumberRepresentation = numberRepresentation
+         .replace(localeConfiguration.thousands, '').replace(localeConfiguration.decimal, '.');
+
+      return cleanNumberRepresentation;
+   } else {
+      console.error(`The number representation does match the given locale.`);
+      return null;
+   }
+}
+
+/**
+ * Gets the actual decimal literal of the given number.
+ * @param {string} numberRepresentation - The initial number representation.
+ * @param {string} locale - The locale id in a string format.
+ * @returns The number in a decimal literal format.
+ */
+function getNumber(numberRepresentation, locale) {
+   if (localeMapper[locale] != null) {
+      const localeConfiguration = localeMapper[locale];
+      const regex = constructExtractionRegex(localeConfiguration.thousands, localeConfiguration.decimal);
+      const cleanNumber = cleanNumberRepresentation(numberRepresentation, regex, localeConfiguration);
+
+      return cleanNumber != null ? parseFloat(cleanNumber) : null;
+   } else {
+      console.error(`Locale: ${locale} is not supported`);
+      return null;
+   }
+}
+
+module.exports = getNumber;
