e81bda1ba68c5ff5abcabfb44c3461927595de0d
George Kamtziridis <georgekam96@gmail.com>
2021-10-20 21:10:34 +0300

    Added automatic integer testing for 'en' locale.

--- src/get-number/specs/get-number.en.spec.js
+++ src/get-number/specs/get-number.en.spec.js
@@ -1,7 +1,8 @@
 const getNumber = require('../get-number');
+const testCaseGenerator = require('./test-case-generator');
 
 describe('Testing `getNumber` with `en` locale', () => {
-  test(`It should return a positive decimal literal when given an 
+  test(`[Manually] It should return a positive decimal literal when given an 
   implicitly positive string representation`, () => {
     expect(getNumber('200', 'en')).toBe(200);
     expect(getNumber('200.45', 'en')).toBe(200.45);
@@ -16,7 +17,7 @@ describe('Testing `getNumber` with `en` locale', () => {
     expect(getNumber('12,054,100.55', 'en')).toBe(12054100.55);
   });
 
-  test(`It should return a positive decimal literal when given an 
+  test(`[Manually] It should return a positive decimal literal when given an 
   explicitly positive string representation`, () => {
     expect(getNumber('+200', 'en')).toBe(200);
     expect(getNumber('+200.45', 'en')).toBe(200.45);
@@ -31,7 +32,7 @@ describe('Testing `getNumber` with `en` locale', () => {
     expect(getNumber('+12,054,100.55', 'en')).toBe(12054100.55);
   });
 
-  test(`It should return a negative decimal literal when given a
+  test(`[Manually] It should return a negative decimal literal when given a
   negative string representation`, () => {
     expect(getNumber('-200', 'en')).toBe(-200);
     expect(getNumber('-200.45', 'en')).toBe(-200.45);
@@ -44,5 +45,109 @@ describe('Testing `getNumber` with `en` locale', () => {
     expect(getNumber('-200,000', 'en')).toBe(-200000);
     expect(getNumber('-2,000,000', 'en')).toBe(-2000000);
     expect(getNumber('-12,054,100.55', 'en')).toBe(-12054100.55);
+  });
+
+  test(`[Automatically][Range: 1 - 100] It should return a possible decimal literal when given an
+  implicitly positive integer string representation`, () => {
+    const testCases = testCaseGenerator('en', 1, 100, 1000);
+    testCases.forEach(testCase => {
+      expect(getNumber(testCase.stringRepresentation, 'en')).toBe(testCase.groundTruth);
+    });
+  });
+
+  test(`[Automatically][Range: 100 - 1.000] It should return a possible decimal literal when given an
+  implicitly positive integer string representation`, () => {
+    const testCases = testCaseGenerator('en', 100, 1000, 1000);
+    testCases.forEach(testCase => {
+      expect(getNumber(testCase.stringRepresentation, 'en')).toBe(testCase.groundTruth);
+    });
+  });
+
+  test(`[Automatically][Range: 1.000 - 10.000] It should return a possible decimal literal when given an
+  implicitly positive integer string representation`, () => {
+    const testCases = testCaseGenerator('en', 1000, 10000, 1000);
+    testCases.forEach(testCase => {
+      expect(getNumber(testCase.stringRepresentation, 'en')).toBe(testCase.groundTruth);
+    });
+  });
+
+  test(`[Automatically][Range: 10.000 - 100.000] It should return a possible decimal literal when given an
+  implicitly positive integer string representation`, () => {
+    const testCases = testCaseGenerator('en', 10000, 100000, 1000);
+    testCases.forEach(testCase => {
+      expect(getNumber(testCase.stringRepresentation, 'en')).toBe(testCase.groundTruth);
+    });
+  });
+
+  test(`[Automatically][Range: 100.000 - 1.000.000] It should return a possible decimal literal when given an
+  implicitly positive integer string representation`, () => {
+    const testCases = testCaseGenerator('en', 100000, 1000000, 1000);
+    testCases.forEach(testCase => {
+      expect(getNumber(testCase.stringRepresentation, 'en')).toBe(testCase.groundTruth);
+    });
+  });
+
+  test(`[Automatically][Range: 1.000.000 - 10.000.000] It should return a possible decimal literal when given an
+  implicitly positive integer string representation`, () => {
+    const testCases = testCaseGenerator('en', 1000000, 10000000, 1000);
+    testCases.forEach(testCase => {
+      expect(getNumber(testCase.stringRepresentation, 'en')).toBe(testCase.groundTruth);
+    });
+  });
+
+  test(`[Automatically][Range: 10.000.000 - 100.000.000] It should return a possible decimal literal when given an
+  implicitly positive integer string representation`, () => {
+    const testCases = testCaseGenerator('en', 10000000, 100000000, 1000);
+    testCases.forEach(testCase => {
+      expect(getNumber(testCase.stringRepresentation, 'en')).toBe(testCase.groundTruth);
+    });
+  });
+
+  test(`[Automatically][Range: 100.000.000 - 1.000.000.000] It should return a possible decimal literal when given an
+  implicitly positive integer string representation`, () => {
+    const testCases = testCaseGenerator('en', 100000000, 1000000000, 1000);
+    testCases.forEach(testCase => {
+      expect(getNumber(testCase.stringRepresentation, 'en')).toBe(testCase.groundTruth);
+    });
+  });
+
+  test(`[Automatically][Range: 1.000.000.000 - 10.000.000.000] It should return a possible decimal literal when given an
+  implicitly positive integer string representation`, () => {
+    const testCases = testCaseGenerator('en', 1000000000, 10000000000, 1000);
+    testCases.forEach(testCase => {
+      expect(getNumber(testCase.stringRepresentation, 'en')).toBe(testCase.groundTruth);
+    });
+  });
+
+  test(`[Automatically][Range: 10.000.000.000 - 100.000.000.000] It should return a possible decimal literal when given an
+  implicitly positive integer string representation`, () => {
+    const testCases = testCaseGenerator('en', 10000000000, 100000000000, 1000);
+    testCases.forEach(testCase => {
+      expect(getNumber(testCase.stringRepresentation, 'en')).toBe(testCase.groundTruth);
+    });
+  });
+
+  test(`[Automatically][Range: 100.000.000.000 - 1.000.000.000.000] It should return a possible decimal literal when given an
+  implicitly positive integer string representation`, () => {
+    const testCases = testCaseGenerator('en', 100000000000, 1000000000000, 1000);
+    testCases.forEach(testCase => {
+      expect(getNumber(testCase.stringRepresentation, 'en')).toBe(testCase.groundTruth);
+    });
+  });
+
+  test(`[Automatically][Range: 1.000.000.000.000 - 10.000.000.000.000] It should return a possible decimal literal when given an
+  implicitly positive integer string representation`, () => {
+    const testCases = testCaseGenerator('en', 1000000000000, 10000000000000, 1000);
+    testCases.forEach(testCase => {
+      expect(getNumber(testCase.stringRepresentation, 'en')).toBe(testCase.groundTruth);
+    });
+  });
+
+  test(`[Automatically][Range: 10.000.000.000.000 - 100.000.000.000.000] It should return a possible decimal literal when given an
+  implicitly positive integer string representation`, () => {
+    const testCases = testCaseGenerator('en', 10000000000000, 100000000000000, 1000);
+    testCases.forEach(testCase => {
+      expect(getNumber(testCase.stringRepresentation, 'en')).toBe(testCase.groundTruth);
+    });
   });
 });
