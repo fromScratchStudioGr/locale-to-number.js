34565f384a9a810af6cf3e4659ca7e8d5ec532a3
George Kamtziridis <georgekam96@gmail.com>
2021-10-26 19:55:02 +0300

    Tested the `fr-be` locale.

--- src/locale-mapper.js
+++ src/locale-mapper.js
@@ -62,6 +62,10 @@ const localeMapper = {
     thousands: '[\\u202F\\u00A0\\u2000\\u2001\\u2003\\s]',
     decimal: '\\,'
   },
+  'fr-be': {
+    thousands: '[\\u202F\\u00A0\\u2000\\u2001\\u2003\\s]',
+    decimal: '\\,'
+  },
   'hi': {
     thousands: '\\,',
     decimal: '\\.',
