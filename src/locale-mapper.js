b61d5562d70687fa9c2d0505311efcc23b2a55b3
George Kamtziridis <georgekam96@gmail.com>
2021-10-26 20:01:02 +0300

    Tested the `fr-ch` locale.

--- src/locale-mapper.js
+++ src/locale-mapper.js
@@ -70,6 +70,10 @@ const localeMapper = {
     thousands: '[\\u202F\\u00A0\\u2000\\u2001\\u2003\\s]',
     decimal: '\\,'
   },
+  'fr-ch': {
+    thousands: '[\\u202F\\u00A0\\u2000\\u2001\\u2003\\s]',
+    decimal: '\\,'
+  },
   'hi': {
     thousands: '\\,',
     decimal: '\\.',
