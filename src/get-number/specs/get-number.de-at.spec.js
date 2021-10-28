ec24a37bb94fb3dc6174dc523df67a4034ed623b
George Kamtziridis <georgekam96@gmail.com>
2021-10-28 18:52:03 +0300

    Updated `de-at` tests.

--- src/get-number/specs/get-number.de-at.spec.js
+++ src/get-number/specs/get-number.de-at.spec.js
@@ -18,13 +18,21 @@ describe('Testing `getNumber` with `de-AT` locale on positive numbers', () => {
     expect(getNumber('200,45', 'de-AT')).toBeCloseTo(200.45, supportedNumberOfFractionDigits);
     expect(getNumber('873,00', 'de-AT')).toBeCloseTo(873.0, supportedNumberOfFractionDigits);
     expect(getNumber('2 050', 'de-AT')).toBeCloseTo(2050, supportedNumberOfFractionDigits);
+    expect(getNumber('2050', 'de-AT')).toBeCloseTo(2050, supportedNumberOfFractionDigits);
     expect(getNumber('2 000,30', 'de-AT')).toBeCloseTo(2000.3, supportedNumberOfFractionDigits);
+    expect(getNumber('2000,30', 'de-AT')).toBeCloseTo(2000.3, supportedNumberOfFractionDigits);
     expect(getNumber('2 342,0', 'de-AT')).toBeCloseTo(2342.0, supportedNumberOfFractionDigits);
+    expect(getNumber('2342,0', 'de-AT')).toBeCloseTo(2342.0, supportedNumberOfFractionDigits);
     expect(getNumber('20 000', 'de-AT')).toBeCloseTo(20000, supportedNumberOfFractionDigits);
+    expect(getNumber('20000', 'de-AT')).toBeCloseTo(20000, supportedNumberOfFractionDigits);
     expect(getNumber('20 000,34', 'de-AT')).toBeCloseTo(20000.34, supportedNumberOfFractionDigits);
+    expect(getNumber('20000,34', 'de-AT')).toBeCloseTo(20000.34, supportedNumberOfFractionDigits);
     expect(getNumber('200 000', 'de-AT')).toBeCloseTo(200000, supportedNumberOfFractionDigits);
+    expect(getNumber('200000', 'de-AT')).toBeCloseTo(200000, supportedNumberOfFractionDigits);
     expect(getNumber('2 000 000', 'de-AT')).toBeCloseTo(2000000, supportedNumberOfFractionDigits);
+    expect(getNumber('2000000', 'de-AT')).toBeCloseTo(2000000, supportedNumberOfFractionDigits);
     expect(getNumber('12 054 100,55', 'de-AT')).toBeCloseTo(12054100.55, supportedNumberOfFractionDigits);
+    expect(getNumber('12054100,55', 'de-AT')).toBeCloseTo(12054100.55, supportedNumberOfFractionDigits);
   });
 
   test(`(Manually) It should return a positive decimal literal when given an 
@@ -37,13 +45,21 @@ describe('Testing `getNumber` with `de-AT` locale on positive numbers', () => {
     expect(getNumber('+200,45', 'de-AT')).toBeCloseTo(200.45, supportedNumberOfFractionDigits);
     expect(getNumber('+873,00', 'de-AT')).toBeCloseTo(873.0, supportedNumberOfFractionDigits);
     expect(getNumber('+2 050', 'de-AT')).toBeCloseTo(2050, supportedNumberOfFractionDigits);
+    expect(getNumber('+2050', 'de-AT')).toBeCloseTo(2050, supportedNumberOfFractionDigits);
     expect(getNumber('+2 000,30', 'de-AT')).toBeCloseTo(2000.3, supportedNumberOfFractionDigits);
+    expect(getNumber('+2000,30', 'de-AT')).toBeCloseTo(2000.3, supportedNumberOfFractionDigits);
     expect(getNumber('+2 342,0', 'de-AT')).toBeCloseTo(2342.0, supportedNumberOfFractionDigits);
+    expect(getNumber('+2342,0', 'de-AT')).toBeCloseTo(2342.0, supportedNumberOfFractionDigits);
     expect(getNumber('+20 000', 'de-AT')).toBeCloseTo(20000, supportedNumberOfFractionDigits);
+    expect(getNumber('+20000', 'de-AT')).toBeCloseTo(20000, supportedNumberOfFractionDigits);
     expect(getNumber('+20 000,34', 'de-AT')).toBeCloseTo(20000.34, supportedNumberOfFractionDigits);
+    expect(getNumber('+20000,34', 'de-AT')).toBeCloseTo(20000.34, supportedNumberOfFractionDigits);
     expect(getNumber('+200 000', 'de-AT')).toBeCloseTo(200000, supportedNumberOfFractionDigits);
+    expect(getNumber('+200000', 'de-AT')).toBeCloseTo(200000, supportedNumberOfFractionDigits);
     expect(getNumber('+2 000 000', 'de-AT')).toBeCloseTo(2000000, supportedNumberOfFractionDigits);
+    expect(getNumber('+2000000', 'de-AT')).toBeCloseTo(2000000, supportedNumberOfFractionDigits);
     expect(getNumber('+12 054 100,55', 'de-AT')).toBeCloseTo(12054100.55, supportedNumberOfFractionDigits);
+    expect(getNumber('+12054100,55', 'de-AT')).toBeCloseTo(12054100.55, supportedNumberOfFractionDigits);
   });
 
   test(`(Automatically)(Range: [0, 1]) It should return a possible decimal literal when given an
@@ -212,13 +228,21 @@ describe('Testing `getNumber` with `de-AT` locale on negative numbers', () => {
     expect(getNumber('-200,45', 'de-AT')).toBeCloseTo(-200.45, supportedNumberOfFractionDigits);
     expect(getNumber('-873,00', 'de-AT')).toBeCloseTo(-873.0, supportedNumberOfFractionDigits);
     expect(getNumber('-2 050', 'de-AT')).toBeCloseTo(-2050, supportedNumberOfFractionDigits);
+    expect(getNumber('-2050', 'de-AT')).toBeCloseTo(-2050, supportedNumberOfFractionDigits);
     expect(getNumber('-2 000,30', 'de-AT')).toBeCloseTo(-2000.3, supportedNumberOfFractionDigits);
+    expect(getNumber('-2000,30', 'de-AT')).toBeCloseTo(-2000.3, supportedNumberOfFractionDigits);
     expect(getNumber('-2 342,0', 'de-AT')).toBeCloseTo(-2342.0, supportedNumberOfFractionDigits);
+    expect(getNumber('-2342,0', 'de-AT')).toBeCloseTo(-2342.0, supportedNumberOfFractionDigits);
     expect(getNumber('-20 000', 'de-AT')).toBeCloseTo(-20000, supportedNumberOfFractionDigits);
+    expect(getNumber('-20000', 'de-AT')).toBeCloseTo(-20000, supportedNumberOfFractionDigits);
     expect(getNumber('-20 000,34', 'de-AT')).toBeCloseTo(-20000.34, supportedNumberOfFractionDigits);
+    expect(getNumber('-20000,34', 'de-AT')).toBeCloseTo(-20000.34, supportedNumberOfFractionDigits);
     expect(getNumber('-200 000', 'de-AT')).toBeCloseTo(-200000, supportedNumberOfFractionDigits);
+    expect(getNumber('-200000', 'de-AT')).toBeCloseTo(-200000, supportedNumberOfFractionDigits);
     expect(getNumber('-2 000 000', 'de-AT')).toBeCloseTo(-2000000, supportedNumberOfFractionDigits);
+    expect(getNumber('-2000000', 'de-AT')).toBeCloseTo(-2000000, supportedNumberOfFractionDigits);
     expect(getNumber('-12 054 100,55', 'de-AT')).toBeCloseTo(-12054100.55, supportedNumberOfFractionDigits);
+    expect(getNumber('-12054100,55', 'de-AT')).toBeCloseTo(-12054100.55, supportedNumberOfFractionDigits);
   });
 
   test(`(Automatically)(Range: [-1, 0]) It should return a possible decimal literal when given an
