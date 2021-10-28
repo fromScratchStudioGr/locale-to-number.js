02a34118c6c86088ac4c054c6b11d8b5ceecb692
George Kamtziridis <georgekam96@gmail.com>
2021-10-28 16:49:16 +0300

    Tested the `es-mx` locale.

--- src/locale-mapper.js
+++ src/locale-mapper.js
@@ -70,6 +70,10 @@ const localeMapper = {
     thousands: '\\.',
     decimal: '\\,',
   },
+  'es-mx': {
+    thousands: '\\,',
+    decimal: '\\.',
+  },
   'fr': {
     thousands: '[\\u202F\\u00A0\\u2000\\u2001\\u2003\\s]',
     decimal: '\\,',
