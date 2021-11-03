e26c04c3f3218ae7f6e018ba680263485c747531
George Kamtziridis <georgekam96@gmail.com>
2021-11-03 21:19:17 +0200

    Tested the `pl-pl` locale.

--- src/locale-mapper.js
+++ src/locale-mapper.js
@@ -184,6 +184,10 @@ const localeMapper = {
     thousands: '[\\u202F\\u00A0\\u2000\\u2001\\u2003\\s]?',
     decimal: '\\,',
   },
+  'pl-pl': {
+    thousands: '[\\u202F\\u00A0\\u2000\\u2001\\u2003\\s]?',
+    decimal: '\\,',
+  },
   'pt': {
     thousands: '\\.?',
     decimal: '\\,',
